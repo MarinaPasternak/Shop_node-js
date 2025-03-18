d49a45bd0084ddce6e30b0525c1c128180131276
Marina_Pasternak <256pasternakmarina@gmail.com>
2025-03-18 23:50:13 +0200

    added cart and cart item modules

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,32 +41,76 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.findAll(products => {
-      const cartProducts = [];
-      for (product of products) {
-        const cartProductData = cart.products.find(
-          prod => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts
-      });
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts().then(
+        products => {
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+          });
+        }).catch
+    })
+    .catch(error => {
+      console.log(error);
     });
-  });
+  // Cart.getCart(cart => {
+  //   Product.findAll(products => {
+  //     const cartProducts = [];
+  //     for (product of products) {
+  //       const cartProductData = cart.products.find(
+  //         prod => prod.id === product.id
+  //       );
+  //       if (cartProductData) {
+  //         cartProducts.push({ productData: product, qty: cartProductData.qty });
+  //       }
+  //     }
+  //     res.render('shop/cart', {
+  //       path: '/cart',
+  //       pageTitle: 'Your Cart',
+  //       products: cartProducts
+  //     });
+  //   });
+  // });
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
+  let fetcedhCart = null;
+  req.user
+  .getCart()
+  .then(cart => {
+    fetcedhCart = cart;
+    return cart.getProducts({where: { id: prodId } });
+  })
+  .then(products => {
+    let product = null;
+
+    if (products.length > 0) {
+      product = products[0];
+
+    }
+
+    let newQuantity = 1;
+
+    if (product) {
+
+    }
+
+    return Product
+      .findByPk(prodId)
+      .then(product => {
+          return fetcedhCart.addProduct(product, {
+            through: { quantity: newQuantity }
+          });
+      })
+      .catch(err => console.log(err))
+  })
+  .catch(err => {
+    console.log(err);
   });
-  res.redirect('/cart');
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
